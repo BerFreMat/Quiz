25843c2cc6f73f508fabd2a0bc3be6f972deb98f
java <java@ubuntu>
2013-01-05 21:40:36 +0100

    factory opkomst

--- src/controller/SQLtestController.java
+++ src/controller/SQLtestController.java
@@ -2,12 +2,19 @@ package controller;
 
 import java.util.List;
 
+import exceptions.ReedsBestaandeOpdrachtException;
+import exceptions.ReedsBestaandeQuizException;
+
 import model.Opdracht;
+import model.OpdrachtCatalogus;
+import model.OpdrachtCategorie;
+import model.OpdrachtSoort;
+import model.Quiz;
 
 import persistency.*;
 import view.*;
 
-public class SQLtestController {
+public class SQLtestController implements DAOFacade {
 
 	private DAOFacade sqldaofacade;
 	private SQLtestFrame sqltestframe;
@@ -18,6 +25,56 @@ public class SQLtestController {
 		
 		sqltestframe = new SQLtestFrame(listopdr);
 		sqltestframe.setVisible(true);
+	}
+
+	@Override
+	public List<Opdracht> getOpdrachten() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Opdracht> getOpdrVanType(OpdrachtSoort opdrachtsoort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Opdracht> getOpdrVanCategorie(
+			OpdrachtCategorie opdrachtCategorie) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void schrijfWeg() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void voegQuizToe(Quiz quiz) throws ReedsBestaandeQuizException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void voegOpdrachtToe(Opdracht opdracht)
+			throws ReedsBestaandeOpdrachtException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public OpdrachtCatalogus getOpdrachtCatalogus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int bepaalLaatsteQuizIDinCatalogus() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
